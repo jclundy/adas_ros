0862f2de9d51bacf95668d24939e1b1760907883
Jaskaran Basson <jaskaranbasson@gmail.com>
2018-04-24 21:48:45 -0400

    add can command to scripts

--- run_live_detect_range.sh
+++ run_live_detect_range.sh
@@ -2,4 +2,5 @@
 xterm -hold -e "roslaunch darknet_ros object_detect.launch" & 
 xterm -hold -e "rosrun image_transport_tutorial udp_publisher" &
 xterm -hold -e "rosbag play /media/nvidia/37d85b64-6ae4-4913-beb8-75f3371b5284/camera_info.bag -l" &
-xterm -hold -e "rosrun lidar_ranging lidar_ranging"
+xterm -hold -e "rosrun lidar_ranging lidar_ranging" &
+xterm -hold -e "rosrun can can"
