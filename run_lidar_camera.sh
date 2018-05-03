5d93b7c3f4e16aed1ae9a2c00c35275e9db53a58
jclundy <sumeetkler@gmail.com>
2018-05-03 16:33:30 -0400

    comment out lidar_ranging in run script

--- run_lidar_camera.sh
+++ run_lidar_camera.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 xterm -hold -e "roslaunch darknet_ros object_detect.launch" & 
-xterm -hold -e "rosbag play /media/nvidia/37d85b64-6ae4-4913-beb8-75f3371b5284/VehAppr6.bag -l" &
+xterm -hold -e "rosbag play /media/nvidia/37d85b64-6ae4-4913-beb8-75f3371b5284/VehAppr4.bag -l" &
 xterm -hold -e "rosbag play /media/nvidia/37d85b64-6ae4-4913-beb8-75f3371b5284/camera_info.bag -l" &
-xterm -hold -e "rosrun lidar_ranging lidar_ranging" &
+#xterm -hold -e "rosrun lidar_ranging lidar_ranging" &
 xterm -hold -e "rosrun can can"
